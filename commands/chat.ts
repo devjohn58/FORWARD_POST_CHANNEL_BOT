d623f98d33ad8229e661a2a302685c09e98abe9b
devjohn58 <devjohn58@gmail.com>
2023-11-20 21:29:56 +0700

    fix command

--- commands/chat.ts
+++ commands/chat.ts
@@ -3,7 +3,7 @@ import { bot } from "..";
 import { showBotActivity } from "../actions/show-bot-activity";
 import { Context } from "grammy";
 let responseError = "You talk too fast. Please say it again!";
-bot.command("/help", async (ctx: Context) => {
+bot.command("help", async (ctx: Context) => {
 	const msg = ctx.update.message;
 	const msgId = msg?.message_id;
 	const chatId = msg?.chat.id;
